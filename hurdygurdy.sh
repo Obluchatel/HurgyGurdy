03110679fa65cc74f7f6fc393fad562708092ceb
Mongoose <38100459+Obluchatel@users.noreply.github.com>
2022-04-05 17:44:02 +0200

    Update hurdygurdy.sh
    Completely rewritten script

--- hurdygurdy.sh
+++ hurdygurdy.sh
@@ -1,23 +1,19 @@
-read -p "Enter string you want to find: " searched_string
-## Variables ##
-list_of_folders_in_root=$(ls /)
-## Functions ##
-check_content_of_folder(){
-    echo $forlders "directory has:"
-    ls /$folders
-}
-## Main ##
-
-for folders in $list_of_folders_in_root
-do 
-if [[ $folders = *srv* ]] || [[ $folders = *proc* ]] || [[ $folders = *mnt* ]] || [[ $folders = *lib* ]] || [[ $folders = *root* ]] || [[ $folders = *lost+found* ]]; then
-    continue
-else
-    if [[ -z $(ls /$folders) ]]; then
-        echo "folder $folders is EMPTY"
-    else 
-        check_content_of_folder $folders
-    fi
-fi
-    
-done
+declare -a array
+array=(1 2 3)
+for string in "${array[@]}"
+    do
+        echo "Looking for" $string "in system files"
+            for folders in $(ls /)
+                do
+                    for file in $(find /$folder -type f -perm -o=r)
+                    do
+                    if grep -r --exclude=*.{png} $string $file
+                    then
+                        echo pattern found in $file
+                    else
+                    continue
+                    fi
+                done
+            done
+        done
+echo DONE, Have a nice day!
